4cfc4ccb5275f75f45bf0ff5360160e7d102ba5f
wyonyango <wycliffealphus7@gmail.com>
2024-08-08 14:01:48 +0300

    feat:add code calling the solving and printing function

--- main.go
+++ main.go
@@ -5,22 +5,28 @@ import (
 	"os"
 
 	"tetris-optimizer/reader"
+	"tetris-optimizer/solver"
 )
 
 func main() {
 	if len(os.Args) != 2 {
 
-		fmt.Println("ERROR")
+		// fmt.Println("ERROR")
+		// return
+		fmt.Fprintln(os.Stderr, "Provide an argument\nUsage: go run . sample.txt | cat -e")
 		return
-		// 		fmt.Fprintf(os.Stderr, `provide an argument
-		// Usage: go run . sample.txt | cat -e`)
-		// 		fmt.Println()
-		// 		os.Exit(0)
 	}
 
 	tetrominoes, err := reader.ReadTetrominoes(os.Args[1])
 	if err != nil {
-		fmt.Println("Error")
+		fmt.Println("ERROR")
 		return
 	}
+
+	solution, err := solver.TetrominoesAssembler(tetrominoes)
+	if err != nil {
+		fmt.Println("ERROR")
+		return
+	}
+	solver.PrintSolution(solution)
 }
