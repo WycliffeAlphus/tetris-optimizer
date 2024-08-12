7efc872953c0bcfe9913f748d1d28538c010ab98
wyonyango <wycliffealphus7@gmail.com>
2024-08-12 13:23:40 +0300

    test:add test file

--- reader/reader_test.go
+++ reader/reader_test.go
@@ -1,1 +1,136 @@
 package reader
+
+import (
+	"os"
+	"testing"
+)
+
+// Helper function to create a temporary file with the given content
+func createTempFile(content string) (string, error) {
+	file, err := os.CreateTemp("", "*.txt")
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	_, err = file.WriteString(content)
+	if err != nil {
+		return "", err
+	}
+	return file.Name(), nil
+}
+
+// Test case for valid tetrominoes
+func TestReadTetrominoes_Valid(t *testing.T) {
+	content := `####
+####
+####
+####
+	
+#...
+#...
+#...
+#...
+
+...#.
+...#.
+.###.
+.....`
+
+	filePath, err := createTempFile(content)
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer os.Remove(filePath)
+
+	_, err = ReadTetrominoes(filePath)
+	if err == nil {
+		t.Fatalf("Expected an error biut got nil")
+	}
+}
+
+// Test case for invalid tetrominoes
+func TestReadTetrominoes_Invalid(t *testing.T) {
+	content := `####
+####
+###
+####
+	
+#...
+#...
+#...
+#...
+
+...#.
+...#.
+.###.
+.....`
+
+	filePath, err := createTempFile(content)
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer os.Remove(filePath)
+
+	_, err = ReadTetrominoes(filePath)
+	if err == nil {
+		t.Fatalf("Expected an error but got nil")
+	}
+}
+
+// Test case for invalid character
+func TestReadTetrominoes_InvalidCharacter(t *testing.T) {
+	content := `####
+####
+#####
+####
+	
+#...
+#...
+#...
+#...
+
+...#.
+...#.
+.###.
+.....`
+
+	filePath, err := createTempFile(content)
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer os.Remove(filePath)
+
+	_, err = ReadTetrominoes(filePath)
+	if err == nil {
+		t.Fatalf("Expected an error but got nil")
+	}
+}
+
+// Test case for tetromino with incorrect size
+func TestReadTetrominoes_IncorrectSize(t *testing.T) {
+	content := `##.#
+####
+##..
+####
+	
+#...
+#...
+#...
+#...
+
+...#.
+...#.
+.###.
+.....`
+
+	filePath, err := createTempFile(content)
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer os.Remove(filePath)
+
+	_, err = ReadTetrominoes(filePath)
+	if err == nil {
+		t.Fatalf("Expected an error but got nil")
+	}
+}
